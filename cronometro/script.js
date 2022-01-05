a9b6559484052eac58879f44f4afcd54e3109dec
Thales Nunes <thales.o.nunes@gmail.com>
2022-01-05 19:01:55 -0300

    Cronômetro Finalizado!

---
+++ cronometro/script.js
@@ -0,0 +1,61 @@
+const stopwatch = document.getElementById('stopwatch');
+const playPauseButton = document.getElementById('play-pause');
+const secondsSphere = document.getElementById('seconds-sphere');
+
+let stopwatchInterval;
+let runningTime = 0;
+
+const playPause = () => {
+    const isPaused = !playPauseButton.classList.contains('running');
+
+    if(isPaused){
+        playPauseButton.classList.add('running');
+        start();
+    }else{
+        playPauseButton.classList.remove('running');
+        pause();
+    }
+}
+
+const pause = () => {
+    //alterando o botão para o estado de pause
+    secondsSphere.style.animationPlayState = 'paused';
+
+    clearInterval(stopwatchInterval);
+}
+
+const stop = () => {
+    // mudando os estilos do botão stop
+    secondsSphere.style.transform = 'rotate(-90deg) translateX(60px)';
+    secondsSphere.style.animation = 'none';
+    playPauseButton.classList.remove('running');
+
+    // resetando ao estado inicial
+    runningTime = 0;
+    clearInterval(stopwatchInterval);
+    stopwatch.textContent = '00:00';
+}
+
+const start = () => {
+    // start na animação
+    secondsSphere.style.animation = 'rotation 60s linear infinite';
+
+    // registrando o tempo corrido
+    let startTime = Date.now() - runningTime;
+    secondsSphere.style.animationPlayState = 'running';
+
+    stopwatchInterval = setInterval(() => {
+        runningTime = Date.now() - startTime;
+        stopwatch.textContent = calculateTime(runningTime);
+    }, 1000)
+}
+
+const calculateTime = (runningTime) => {
+    const total_seconds = Math.floor(runningTime/1000);
+    const total_minutes = Math.floor(total_seconds/60);
+
+    const display_seconds = (total_seconds % 60).toString().padStart(2, "0");
+    const display_minutes = total_minutes.toString().padStart(2, "0");
+
+    return `${display_minutes}:${display_seconds}`;
+}
