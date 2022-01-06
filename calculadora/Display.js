81338d2f4f0fcc0a43f2c53e7bd24498c264a06d
Thales Nunes <thales.o.nunes@gmail.com>
2022-01-06 17:22:48 -0300

    Calculadora finalizada

---
+++ calculadora/Display.js
@@ -0,0 +1,61 @@
+class Display {
+    constructor(displayValorAnterior, displayValorAtual) {
+        this.displayValorAtual = displayValorAtual;
+        this.displayValorAnterior = displayValorAnterior;
+        this.calculadora = new Calculadora();
+        this.tipoOperacao = undefined;
+        this.valorAtual = '';
+        this.valorAnterior = '';
+        this.sinais = {
+            somar : "+",
+            subtrair : "-",
+            multiplicar : "x",
+            dividir : "/",
+        };
+    }
+
+    apagarCaractere(){
+        this.valorAtual = this.valorAtual.toString().slice(0, -1);
+        this.imprimirValores();
+    }
+
+    limparDisplay(){
+        this.valorAtual = "";
+        this.valorAnterior = "";
+        this.tipoOperacao = undefined;
+        this.imprimirValores();
+    }
+
+    computar(tipo){
+        this.tipoOperacao !== "igual" && this.calcular();
+        this.tipoOperacao = tipo;
+        this.valorAnterior = this.valorAtual || this.valorAnterior;
+        this.valorAtual = "";
+        this.imprimirValores();
+    }
+
+    agregarNumero(numero) {
+        // tratando a questão do ponto
+        // só se pode ter um unico ponto
+        if(numero === '.' && this.valorAtual.includes('.')){
+            return
+        }else{
+            this.valorAtual = this.valorAtual.toString() + numero.toString();
+            this.imprimirValores();
+        }       
+    }
+
+    imprimirValores() {
+        this.displayValorAtual.textContent = this.valorAtual;
+        this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.sinais[this.tipoOperacao] || ''}`;
+    }
+
+    calcular(){
+        const valorAnterior = parseFloat(this.valorAnterior);
+        const valorAtual = parseFloat(this.valorAtual);
+
+        if(isNaN(valorAtual) || isNaN(valorAnterior)) return // se for NaN o método é quebrado
+
+        this.valorAtual = this.calculadora[this.tipoOperacao](valorAnterior, valorAtual)
+    }
+}
---
+++ calculadora/script.js
@@ -0,0 +1,23 @@
+// Referenciando todos os elementos necessários para a manipulação da calculadora
+const displayValorAnterior = document.getElementById("valor-anterior");
+const displayValorAtual = document.getElementById("valor-atual");
+const botoesNumeros = document.querySelectorAll(".numero");
+const botoesOperadores = document.querySelectorAll(".operador");
+
+const display = new Display(displayValorAnterior, displayValorAtual);
+
+// percorrerá todos os botões com classe ".numero" adicionando um eventListener
+// que ao clicar, será passado o texto de cada botão para o "agregarNumero()" da classe Display
+botoesNumeros.forEach(botao => {
+    botao.addEventListener('click', () => {
+        display.agregarNumero(botao.innerHTML);
+    });
+});
+
+// percorrerá todos os botões com classe ".operador" adicionando um eventListener
+// que ao clicar, será passado o valor de cada botão para o método "computar()" classe Display.
+botoesOperadores.forEach(botao => {
+    botao.addEventListener('click', () => {
+        display.computar(botao.value);
+    });
+});
